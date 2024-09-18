fb0975b6928ae2c9207a7fffc49515687b604d55
lucas cardoso <steam2.22042001@hotmail.com>
2024-09-18 14:51:48 -0300

    alterações nas imagens : initial commit

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-function toggleMode(){
+function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 
- //pegar a img
+  //pegar a img
   const img = document.querySelector("#profile img")
-   if(html.classList.contains('light')) {
-   img.setAttribute('src','./assets/Avatar-light.png')
-    img.setAttribute('alt', "foto de mike com fundo azul")
-  } 
- else {
-   img.setAttribute('src','./assets/Avatar.png')
-   img.setAttribute('alt',"Foto de Lucas Cardoso, usando camisa preta, barba e fundo")
-   
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/Avatargato-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de uam gato branco cartoon, usando oculos escuro  e fundo azul"
+    )
+  } else {
+    img.setAttribute("src", "./assets/Avatargato.png")
+    img.setAttribute(
+      "alt",
+      "Foto de uam gato branco cartoon, usando oculos escuro quadrado e fundo amarelo"
+    )
   }
-
-
-
 }
