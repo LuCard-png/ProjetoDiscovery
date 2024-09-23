ec8cec60c8fca1080994d40487b5d53b5a79e0e0
lucas cardoso <steam2.22042001@hotmail.com>
2024-09-23 18:53:35 -0300

    att img 2.0

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function toggleMode() {
   //pegar a img
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Avatar.png")
     img.setAttribute(
       "alt",
       "Foto de Lucas Cardoso, usando camisa preta, barba e fundo"
     )
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Avatar.png")
     img.setAttribute(
       "alt",
       "Foto de Lucas Cardoso, usando camisa preta, barba e fundo"
